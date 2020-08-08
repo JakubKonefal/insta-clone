946cc4b18ca1e0d06504aa90adf3b6f6526a2f35
Jakub Konefał <konefaljakub@gmail.com>
2020-08-08 12:00:34 +0200

    - Add redux store - Create root reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import SignIn from './SignIn/SignIn';
 import SignUp from './SignUp/SignUp';
 import classes from './App.module.css';
+import store from './store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <div className={classes.App__LoginWraper}>
         <Route path="/signup" component={SignUp} />
         <Route path="/" exact component={SignIn} />
       </div>
-    </>
+    </Provider>
   );
 }
 
